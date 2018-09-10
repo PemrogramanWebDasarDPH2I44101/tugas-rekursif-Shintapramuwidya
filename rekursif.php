d86777f6947399949d04a6a140c691cd1b94bf18
Shintapramuwidya <42725343+Shintapramuwidya@users.noreply.github.com>
2018-09-10 18:33:47 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,40 @@
 <?php
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+$k = 0;
+rekursif1(5);
+
+function rekursif2($l, $kolom){
+global $k;
+if ($l > $k - $kolom) {
+	echo "*";
+	rekursif2($l - 1, $kolom);
+ }
 }
-rekursif(5);
+function rekursif1($kolom){
+	global $k;
+	rekursif2($k, $kolom);
+	echo "<br>";
+	if (--$kolom > $k) {
+		rekursif1($kolom);
+	}
+}
+
+echo "/*=================================================*/<br>";
+
+$n=5;
+function rekursif($i,$j,$n){
+if ($i<= $n) {
+		if($j<=$i){
+			echo $j;
+			$j++;
+			rekursif($i,$j,$n);
+		}
+		else{
+			echo "<br>";
+		$i++;
+		rekursif($i,1,$n);
+		}
+	}
+}
+	rekursif(1,1,$n);
 ?>
